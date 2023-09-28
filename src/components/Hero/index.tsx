e4d371531a8aa90c4b1fbeff1fcff04275ec9299
cleiton lahm <cleijllahm@gmail.com>
2023-09-28 10:56:04 -0300

    mudando todo o css para styled components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { Form, FormDiv, TituloH2 } from './Hero.module'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <Form>
+    <FormDiv>
+      <TituloH2>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </TituloH2>
+    </FormDiv>
+  </Form>
 )
 
 export default Hero
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import GlobalStyles from './global'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
+    <GlobalStyles></GlobalStyles>
     <App />
   </React.StrictMode>
 )
